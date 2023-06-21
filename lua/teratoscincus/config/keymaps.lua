051103958551171bd394c11bda1299a8d4fc44c2
Max <teratoscincus@hotmail.com>
2023-06-21 12:34:00 +0200

    Extend keymaps

--- lua/teratoscincus/config/keymaps.lua
+++ lua/teratoscincus/config/keymaps.lua
@@ -1,7 +1,15 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>ex", vim.cmd.Ex) -- Go to (ex)plorer
+local key = vim.keymap.set
+
+-- Disable defaults
+key("n", "<CR>", "<Nop>") -- Prevent moving down on accidentay ENTER
+
+-- Quality of life bindings
+key("n", "<leader>ex", vim.cmd.Ex) -- Go to (ex)plorer
+key("n", "<leader>w", ":w")
+key("n", "<leader>q", ":q")
 
 -- Move blocks of code in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv") -- FIX: Does not work with more than one line
+key("v", "J", ":m '>+1<CR>gv=gv")
+key("v", "K", ":m '>-2<CR>gv=gv") -- FIX: Does not work with more than one line
