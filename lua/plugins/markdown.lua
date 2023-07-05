eb07df87a706611007a24e3ad48475e81609e34e
Max <teratoscincus@hotmail.com>
2023-07-05 12:06:29 +0200

    Add keymaps for markdown-preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -4,5 +4,18 @@ return {
     ft = "markdown",
     event = "BufRead",
     build = "cd app && npm install",
+    config = function()
+      local map = vim.keymap.set
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>om"] = {
+          name = "+markdown",
+        },
+      })
+
+      -- Keymaps
+      map("n", "<leader>omp", "<cmd>MarkdownPreview<cr>", { desc = "Markdown preview" })
+      map("n", "<leader>oms", "<cmd>MarkdownPreviewStop<cr>", { desc = "Stop markdown preview" })
+    end,
   },
 }
